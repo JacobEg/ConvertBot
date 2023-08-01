b9fbd20f9a83c91bb8f04668fb719878ca59bde7
JacobEg <jacob.egestad@live.com>
2023-08-01 16:14:17 -0700

    Created DiscordBot and requirements.txt and updated README

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # ConvertBot
 A discord bot for automatically converting from metric to imperial and vice-versa
 <hl>
-Python version: 3.10.5
+## Versions
+Python version: 3.11.4
+requests: 2.31.0
+<hl>
+## Usage
+bot_handle number source_unit converted_unit
--- connect_to_discord.py
+++ connect_to_discord.py
@@ -2,4 +2,21 @@
 For setting up the connection to the discord server.
 '''
 
+# stdlib imports
+# import json
+
+# PyPi module imports
+import requests
+
+# my imports
+from constants import BOT_HANDLE
+
+class DiscordBot:
+    def __init__():
+        pass
+    def write_msg(msg: str):
+        pass
+    def read_next_prompt() -> str:
+        pass
+
 discord = None
