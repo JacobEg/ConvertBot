c16d17af8f2ca88e77847ba35d30b9c0e1ae2b9f
JacobEg <jacob.egestad@live.com>
2023-10-17 13:33:16 -0700

    Skeleton of vol and consts

--- conversion/volume.py
+++ conversion/volume.py
@@ -1,3 +1,34 @@
 '''
 Implementing volume conversions.
 '''
+
+from abc import ABC
+
+class Volume(ABC):
+    def __init__(self, vol: float):
+        super().__init__()
+        self._vol = vol
+    
+    def get_volume(self) -> float:
+        return self._vol
+    
+class Liter(Volume):
+    pass
+
+class Gallon(Volume):
+    pass
+
+class Quart(Volume):
+    pass
+
+class Pint(Volume):
+    pass
+
+class Cup(Volume):
+    pass
+
+class Tablespoon(Volume):
+    pass
+
+class Teaspoon(Volume):
+    pass
